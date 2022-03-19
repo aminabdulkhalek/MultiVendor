1c4a8a123e1f5f8d9567ca44d47e65ba1650b982
aminabdulkhalek <abdulkhalek632@gmail.com>
2022-03-19 23:51:50 +0200

    moved vendor components into seperat file

--- MultiVendor-FrontEnd/src/app/app-routing.module.ts
+++ MultiVendor-FrontEnd/src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { VendorDashboardComponent } from './vendor-dashboard/vendor-dashboard.component';
-import { VendorProductsComponent } from './vendor-products/vendor-products.component';
+import { VendorDashboardComponent } from './vendor/vendor-dashboard/vendor-dashboard.component';
+import { VendorProductsComponent } from './vendor/vendor-products/vendor-products.component';
 
 
 
