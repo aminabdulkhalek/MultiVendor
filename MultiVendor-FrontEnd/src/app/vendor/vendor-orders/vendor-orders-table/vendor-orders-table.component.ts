0790c21c1912729cb8ca8590319bffaebbaa998b
aminabdulkhalek <abdulkhalek632@gmail.com>
2022-03-20 13:02:40 +0200

    bug removed

--- MultiVendor-FrontEnd/src/app/vendor/vendor-orders/vendor-orders-table/vendor-orders-table.component.ts
+++ MultiVendor-FrontEnd/src/app/vendor/vendor-orders/vendor-orders-table/vendor-orders-table.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
-import { Products } from '../../vendor-products/products/products.component';
 
 export interface Orders {
   id: number;
