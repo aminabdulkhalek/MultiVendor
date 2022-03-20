ffbafe957acce620a82eeddc3e58958bc865fec1
aminabdulkhalek <abdulkhalek632@gmail.com>
2022-03-20 17:00:19 +0200

    vendor settings: added main layout(navbar + sidebar)

--- MultiVendor-FrontEnd/src/app/vendor/vendor-settings/vendor-settings.component.ts
+++ MultiVendor-FrontEnd/src/app/vendor/vendor-settings/vendor-settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faSignOut, faBell } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-vendor-settings',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vendor-settings.component.scss']
 })
 export class VendorSettingsComponent implements OnInit {
-
+  logout = faSignOut;
+  notification = faBell;
+  imagePath = "favicon.ico";
   constructor() { }
 
   ngOnInit(): void {
