a58f4741e430c39a6504a72de9c01517bffdd8e5
aminabdulkhalek <abdulkhalek632@gmail.com>
2022-03-23 23:11:56 +0200

    added orders table migration

---
+++ MultiVendor-BackEnd/database/migrations/2022_03_23_203732_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('customer_id')->unsigned()->index();
+            $table->foreign('customer_id')->references('id')->on('customers')->onDelete('cascade');
+            $table->bigInteger('billing_id')->unsigned()->index();
+            $table->foreign('billing_id')->references('id')->on('billinginfo')->onDelete('cascade');
+            $table->timestamps();
+            $table->bigInteger('total_price');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
