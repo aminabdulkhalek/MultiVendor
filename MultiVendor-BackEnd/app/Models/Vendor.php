ec892a1e46ffcf49ee6d75565aca19449894c4b5
aminabdulkhalek <abdulkhalek632@gmail.com>
2022-03-24 01:42:38 +0200

    created vendor model and added relations

---
+++ MultiVendor-BackEnd/app/Models/Vendor.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class vendor extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'address',
+        'phone',
+        'logo',
+        'commission_rate',
+        'banner',
+        'instagram_link',
+        'facebook_link',
+        'twitter_link',
+    ];
+
+    /**
+     * Get all of the products for the vendor
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function products() : HasMany
+    {
+        return $this->hasMany(Products::class, 'vendor_id');
+    }
+    /**
+     * Get all of the flags for the vendor
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function flags(): HasMany
+    {
+        return $this->hasMany(VendorFlags::class, 'vendor_id');
+    }
+    /**
+     * Get the balance associated with the vendor
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function balance(): HasOne
+    {
+        return $this->hasOne(Balance::class, 'vendor_id');
+    }
+}
