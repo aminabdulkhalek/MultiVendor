2c939443514bd08ca3f8618125c67bada22d6301
aminabdulkhalek <abdulkhalek632@gmail.com>
2022-03-23 22:30:49 +0200

    created customers migration

---
+++ MultiVendor-BackEnd/database/migrations/2022_03_23_202644_create_customers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned()->index();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('country', 255)->nullable();
+            $table->string('state', 255)->nullable();
+            $table->dateTime('dat_of_birth')->nullable();
+            $table->string('gender', 100)->nullable();
+            $table->bigInteger('Tax_number')->nullable()->default(123456);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
