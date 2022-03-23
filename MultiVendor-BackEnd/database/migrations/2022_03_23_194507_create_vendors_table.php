a9e73790ea78e7b48590c5bc8c65d43145f1b1d5
aminabdulkhalek <abdulkhalek632@gmail.com>
2022-03-23 22:09:22 +0200

    created vendors migration

---
+++ MultiVendor-BackEnd/database/migrations/2022_03_23_194507_create_vendors_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVendorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vendors', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned()->index();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('address');
+            $table->bigInteger('phone')->unique();
+            $table->string('logo');
+            $table->string('banner');
+            $table->string('instagram_link')->nullable();
+            $table->string('facebook_link')->nullable();
+            $table->string('twitter_link')->nullable();
+            $table->tinyInteger('commission_rate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vendors');
+    }
+}
