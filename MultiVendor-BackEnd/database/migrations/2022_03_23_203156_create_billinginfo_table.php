19e3f4a796b75320c97c41f07159b22d6a6c22f3
aminabdulkhalek <abdulkhalek632@gmail.com>
2022-03-23 22:36:48 +0200

    created billing info migration

---
+++ MultiVendor-BackEnd/database/migrations/2022_03_23_203156_create_billinginfo_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBillinginfoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('billinginfo', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('customer_id')->unsigned()->index();
+            $table->foreign('customer_id')->references('id')->on('customers')->onDelete('cascade');
+            $table->string('country', 100);
+            $table->string('street_address', 100);
+            $table->string('city', 100);
+            $table->string('state', 100);
+            $table->string('email', 100);
+            $table->bigInteger('zip_code')->nullable()->default(0000);
+            $table->bigInteger('phone');
+            $table->text('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('billinginfo');
+    }
+}
