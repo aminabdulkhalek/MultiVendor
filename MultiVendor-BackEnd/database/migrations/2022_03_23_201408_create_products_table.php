e1bcadf79166a565135e2632e57d905675a35211
aminabdulkhalek <abdulkhalek632@gmail.com>
2022-03-23 22:19:49 +0200

    created products migration

---
+++ MultiVendor-BackEnd/database/migrations/2022_03_23_201408_create_products_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('vendor_id')->references('id')->on('vendors')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('category')->onDelete('cascade');
+            $table->bigInteger('price');
+            $table->bigInteger('stock');
+            $table->tinyInteger('status');
+            $table->string('type', 255)->default('simple');
+            $table->text('feature1')->nullable();
+            $table->text('feature2')->nullable();
+            $table->text('feature3')->nullable();
+            $table->string('img1', 255)->nullable();
+            $table->string('img2', 255)->nullable();
+            $table->string('img3', 255)->nullable();
+            $table->string('img4', 255)->nullable();
+            $table->text('desc1')->nullable();
+            $table->text('desc2')->nullable();
+
+
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
