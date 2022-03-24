3b48fc5c40f4dbe121dee7e98f2f1a78d32e9161
aminabdulkhalek <abdulkhalek632@gmail.com>
2022-03-24 02:14:48 +0200

    created category model and added relations

---
+++ MultiVendor-BackEnd/app/Models/Category.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'category_name'
+    ];
+
+    /**
+     * Get all of the products for the Category
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function products(): HasMany
+    {
+        return $this->hasMany(Products::class,'category_id');
+    }
+}
