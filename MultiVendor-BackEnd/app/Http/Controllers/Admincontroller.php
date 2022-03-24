f3e70d4777ac1619304b853dc5d209c7cce17162
aminabdulkhalek <abdulkhalek632@gmail.com>
2022-03-24 03:23:45 +0200

    post api update admin info

--- MultiVendor-BackEnd/app/Http/Controllers/Admincontroller.php
+++ MultiVendor-BackEnd/app/Http/Controllers/Admincontroller.php
@@ -6,10 +6,25 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use App\Models\Customer;
+use App\Models\Admin;
 
 class Admincontroller extends Controller
 {
     public function getCustomers(){
         return $customers = Customer::get();
+    }
+
+
+    public function updateInfo(Request $request){
+        $admin = new Admin;
+        $admin ->user_id = Auth::user()->id;
+        $admin ->total_balance = $request ->total_balance;
+        $admin ->income = $request->income ;
+        $admin ->save();
+
+        return response()->json([
+            'message' => 'Admin info Updated',
+            'admin' => $admin,
+        ], 201);
     }
 }
