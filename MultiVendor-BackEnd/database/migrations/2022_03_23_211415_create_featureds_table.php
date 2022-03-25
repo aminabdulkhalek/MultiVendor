b900d6a87e31cb41eb26517c4e76c116b8e43a85
aminabdulkhalek <abdulkhalek632@gmail.com>
2022-03-25 03:57:01 +0200

    fixed migration

--- MultiVendor-BackEnd/database/migrations/2022_03_23_211415_create_featureds_table.php
+++ MultiVendor-BackEnd/database/migrations/2022_03_23_211415_create_featureds_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFeaturedTable extends Migration
+class CreateFeaturedsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateFeaturedTable extends Migration
      */
     public function up()
     {
-        Schema::create('featured', function (Blueprint $table) {
+        Schema::create('featureds', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('vendor_id')->unsigned()->index();;
             $table->foreign('vendor_id')->references('id')->on('vendors')->onDelete('cascade');
@@ -28,6 +28,6 @@ class CreateFeaturedTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('featured');
+        Schema::dropIfExists('featureds');
     }
 }
