a7fa8281bfe3eaa749dd275c0062160eb0647924
aminabdulkhalek <abdulkhalek632@gmail.com>
2022-03-28 18:04:15 +0300

    added 4 get http requests to admin dashboard

--- MultiVendor-FrontEnd/src/app/admin/admin-dashboard/orders-box/orders-box.component.ts
+++ MultiVendor-FrontEnd/src/app/admin/admin-dashboard/orders-box/orders-box.component.ts
@@ -9,20 +9,53 @@ import { HttpClient } from '@angular/common/http';
 export class OrdersBoxComponent implements OnInit {
   orders;
   errorMessage;
+  pending;
+  approved;
+  disapproved;
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    const headers = { 'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3Q6ODAwMFwvYXBpXC9hdXRoXC9sb2dpbiIsImlhdCI6MTY0ODQ3ODA3MCwiZXhwIjoxNjQ4NDgxNjcwLCJuYmYiOjE2NDg0NzgwNzAsImp0aSI6IjR0MFI5OXVOZThaSWQ4eFEiLCJzdWIiOjEsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.IBraaqwUIqpGwlNl_bEkHzhOGRThsmav1T1B3FpcWkM' }
-    this.http.get<any>('http://localhost:8000/api/admin/nb-orders',{headers}).subscribe({
-        next: data => {
-            this.orders = data.nbOrders;
-            console.log(data.nbOrders)
-        },
-        error: error => {
-            this.errorMessage = error.message;
-            console.error('There was an error!', this.errorMessage);
-        }
+    const headers = { 'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3Q6ODAwMFwvYXBpXC9hdXRoXC9sb2dpbiIsImlhdCI6MTY0ODQ3ODA3MCwiZXhwIjoxNjQ4NDgxNjcwLCJuYmYiOjE2NDg0NzgwNzAsImp0aSI6IjR0MFI5OXVOZThaSWQ4eFEiLCJzdWIiOjEsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.IBraaqwUIqpGwlNl_bEkHzhOGRThsmav1T1B3FpcWkM' };
+    this.http.get<any>('http://localhost:8000/api/admin/nb-orders', { headers }).subscribe({
+      next: data => {
+        this.orders = data.nbOrders;
+      },
+      error: error => {
+        this.errorMessage = error.message;
+        console.error('There was an error!', this.errorMessage);
+      }
     })
+
+    this.http.get<any>('http://localhost:8000/api/admin/nb-pending-orders', { headers }).subscribe({
+      next: data => {
+        this.pending = data.pending_orders*100/this.orders;
+      },
+      error: error => {
+        this.errorMessage = error.message;
+        console.error('There was an error!', this.errorMessage);
+      }
+    })
+
+    this.http.get<any>('http://localhost:8000/api/admin/nb-approved-orders', { headers }).subscribe({
+      next: data => {
+        this.approved = data.approved_orders*100/this.orders;
+      },
+      error: error => {
+        this.errorMessage = error.message;
+        console.error('There was an error!', this.errorMessage);
+      }
+    })
+
+    this.http.get<any>('http://localhost:8000/api/admin/nb-disapproved-orders', { headers }).subscribe({
+      next: data => {
+        this.disapproved = data.disapproved_orders*100/this.orders;
+      },
+      error: error => {
+        this.errorMessage = error.message;
+        console.error('There was an error!', this.errorMessage);
+      }
+    })
+
   }
 
 }
