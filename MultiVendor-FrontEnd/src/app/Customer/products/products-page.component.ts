99196adad72c54b8c26524b1884c662d3e874454
aminabdulkhalek <abdulkhalek632@gmail.com>
2022-03-29 19:56:32 +0300

    adding two section for products page

--- MultiVendor-FrontEnd/src/app/Customer/products/products-page.component.ts
+++ MultiVendor-FrontEnd/src/app/Customer/products/products-page.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { faSignOut, faUserCircle, faHeart, faShoppingCart, faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { faSignOut, faUserCircle, faHeart, faShoppingCart, faShoppingBasket, faListSquares, faList, faTableList, faListOl, faList12, faClipboardList, faGripLinesVertical, faGripLines, faDisplay, faBoxes, faThList, faChessBoard } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-  selector: 'app-products',
+  selector: 'app-products-page',
   templateUrl: './products-page.component.html',
   styleUrls: ['./products-page.component.scss']
 })
@@ -12,7 +13,12 @@ export class ProductsPageComponent implements OnInit {
   wishlist = faHeart;
   cart= faShoppingCart;
   shopping_cart= faShoppingBasket;
+  products;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
   constructor() { }
+  ngAfterViewInit() {
+    this.products.paginator = this.paginator;
+  }
 
   ngOnInit(): void {
   }
