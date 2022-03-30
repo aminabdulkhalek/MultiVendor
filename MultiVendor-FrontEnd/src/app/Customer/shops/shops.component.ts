265ba75e6e8a7e1a06320a5bd8188ac7b835e158
aminabdulkhalek <abdulkhalek632@gmail.com>
2022-03-30 03:11:19 +0300

    shop component: parent container set to flex added loop over shops

--- MultiVendor-FrontEnd/src/app/Customer/shops/shops.component.ts
+++ MultiVendor-FrontEnd/src/app/Customer/shops/shops.component.ts
@@ -12,6 +12,7 @@ export class ShopsComponent implements OnInit {
   wishlist = faHeart;
   cart= faShoppingCart;
   shopping_cart= faShoppingBasket;
+  shops=[1,2,3,4,5,6,7,8,9]
   constructor() { }
 
   ngOnInit(): void {
