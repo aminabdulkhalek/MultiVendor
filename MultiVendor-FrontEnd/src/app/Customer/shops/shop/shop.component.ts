50e03ad2e964a2cc87edc75721a81e8a18173b38
aminabdulkhalek <abdulkhalek632@gmail.com>
2022-03-30 03:05:45 +0300

    shop component: filled and styled

--- MultiVendor-FrontEnd/src/app/Customer/shops/shop/shop.component.ts
+++ MultiVendor-FrontEnd/src/app/Customer/shops/shop/shop.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faPhone, faStar, faStarHalf } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-shop',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shop.component.scss']
 })
 export class ShopComponent implements OnInit {
-
+  phone = faPhone;
+  star = faStar;
+  half_star = faStarHalf
   constructor() { }
 
   ngOnInit(): void {
