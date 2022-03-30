16b175d718866a6669488095c5bd78c8fe47fc77
aminabdulkhalek <abdulkhalek632@gmail.com>
2022-03-30 07:41:48 +0300

    shop cmponent: added profile preview section

--- MultiVendor-FrontEnd/src/app/Customer/shop/shop.component.ts
+++ MultiVendor-FrontEnd/src/app/Customer/shop/shop.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { faSignOut, faUserCircle, faHeart, faShoppingCart, faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
+import { faSignOut, faUserCircle, faHeart, faShoppingCart, faShoppingBasket, faPhone, faStar, faStarHalf } from '@fortawesome/free-solid-svg-icons';
+import { MatIconRegistry } from '@angular/material/icon';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-shop',
@@ -7,13 +9,35 @@ import { faSignOut, faUserCircle, faHeart, faShoppingCart, faShoppingBasket } fr
   styleUrls: ['./shop.component.scss']
 })
 export class ShopComponent implements OnInit {
-  logout=faSignOut;
+  logout = faSignOut;
   account = faUserCircle;
   wishlist = faHeart;
-  cart= faShoppingCart;
-  shopping_cart= faShoppingBasket;
-
-  constructor() { }
+  cart = faShoppingCart;
+  shopping_cart = faShoppingBasket;
+  phone = faPhone
+  star = faStar;
+  half_star = faStarHalf;
+  constructor(
+    private matIconRegistry:MatIconRegistry,
+    private domSanitzer:DomSanitizer,
+  ) { 
+    this.matIconRegistry.addSvgIcon(
+      'facebook',
+      this.domSanitzer.bypassSecurityTrustResourceUrl('/assets/socail_media_icon/facebook.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'instagram',
+      this.domSanitzer.bypassSecurityTrustResourceUrl('/assets/socail_media_icon/instagram.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'twitter',
+      this.domSanitzer.bypassSecurityTrustResourceUrl('/assets/socail_media_icon/twitter.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'linkedin',
+      this.domSanitzer.bypassSecurityTrustResourceUrl('/assets/socail_media_icon/linkedin.svg')
+    );
+  }
 
   ngOnInit(): void {
   }
