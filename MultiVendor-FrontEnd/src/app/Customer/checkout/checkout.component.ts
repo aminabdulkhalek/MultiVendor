a5ca09252bff35db3021ad250b7a8d3599dbe836
aminabdulkhalek <abdulkhalek632@gmail.com>
2022-03-31 09:04:04 +0300

    added main layout to checkout

--- MultiVendor-FrontEnd/src/app/Customer/checkout/checkout.component.ts
+++ MultiVendor-FrontEnd/src/app/Customer/checkout/checkout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faSignOut, faUserCircle, faHeart, faShoppingCart, faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-checkout',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.component.scss']
 })
 export class CheckoutComponent implements OnInit {
-
+  logout = faSignOut;
+  account = faUserCircle;
+  wishlist = faHeart;
+  cart = faShoppingCart;
+  shopping_cart = faShoppingBasket;
   constructor() { }
 
   ngOnInit(): void {
