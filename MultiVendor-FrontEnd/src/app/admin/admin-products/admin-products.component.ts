b4d7891c2b26f1e695cfa172589f0adad55951c5
aminabdulkhalek <abdulkhalek632@gmail.com>
2022-04-02 00:36:45 +0300

    admin products: added intial auth check

--- MultiVendor-FrontEnd/src/app/admin/admin-products/admin-products.component.ts
+++ MultiVendor-FrontEnd/src/app/admin/admin-products/admin-products.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { faSignOut, faBell } from '@fortawesome/free-solid-svg-icons';
+import { AuthStateService } from 'src/app/shared/auth-state.service';
+import { AuthService } from 'src/app/shared/auth.service';
+import { TokenService } from 'src/app/shared/token.service';
 
 @Component({
   selector: 'app-admin-products',
@@ -9,9 +13,30 @@ import { faSignOut, faBell } from '@fortawesome/free-solid-svg-icons';
 export class AdminProductsComponent implements OnInit {
   logout = faSignOut;
   notification = faBell;
-  constructor() { }
+  isSignedIn!: boolean;
+
+  constructor(
+    private auth: AuthStateService,
+    public router: Router,
+    public token: TokenService,
+    public authService :AuthService
+
+  ) { }
 
   ngOnInit(): void {
+    this.auth.userAuthState.subscribe((val) => {
+      this.isSignedIn = val;
+      if (!val) {
+        this.router.navigate(['login']);
+      }
+    });
+    if (localStorage.getItem('user_type') != '0'){
+      localStorage.removeItem('user_type');
+      this.signOut()
+    }
+  }
+  signOut() {
+    this.authService.signOut();
   }
 
 }
