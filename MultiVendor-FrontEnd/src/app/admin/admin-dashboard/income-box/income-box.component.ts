5adabf920fc9d660b75f708054744a415867d2da
aminabdulkhalek <abdulkhalek632@gmail.com>
2022-04-02 00:10:11 +0300

    connected admin income box to the backend

--- MultiVendor-FrontEnd/src/app/admin/admin-dashboard/income-box/income-box.component.ts
+++ MultiVendor-FrontEnd/src/app/admin/admin-dashboard/income-box/income-box.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { API_URL } from 'src/app/shared/auth.service';
 
 @Component({
   selector: 'app-income-box',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./income-box.component.scss']
 })
 export class IncomeBoxComponent implements OnInit {
-
-  constructor() { }
+  sales;
+  income;
+  errorMessage;
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.getinfo()
   }
-
+  getinfo(){
+    this.http.get<any>(API_URL+'admin/total-sales').subscribe({
+      next: data => {
+        this.sales = data.total_sales;
+      },
+      error: error => {
+        this.errorMessage = error.message;
+        console.error('There was an error!', this.errorMessage);
+      }
+    })
+    this.http.get<any>(API_URL+'admin/total-income').subscribe({
+      next: data => {
+        this.income = data.total_income;
+      },
+      error: error => {
+        this.errorMessage = error.message;
+        console.error('There was an error!', this.errorMessage);
+      }
+    })
+  }
 }
