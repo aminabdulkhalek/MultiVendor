371f1079aaf4be042bf5b4eeacd76d84315f99b0
aminabdulkhalek <abdulkhalek632@gmail.com>
2022-04-02 15:29:21 +0300

    admin vendors: get name and display it

--- MultiVendor-FrontEnd/src/app/admin/admin-dashboard/admin-dashboard.component.ts
+++ MultiVendor-FrontEnd/src/app/admin/admin-dashboard/admin-dashboard.component.ts
@@ -1,8 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { faSignOut, faBell } from '@fortawesome/free-solid-svg-icons';
 import { AuthStateService } from 'src/app/shared/auth-state.service';
-import { AuthService } from 'src/app/shared/auth.service';
+import { API_URL, AuthService } from 'src/app/shared/auth.service';
 import { TokenService } from 'src/app/shared/token.service';
 
 @Component({
@@ -16,12 +17,16 @@ export class AdminDashboardComponent implements OnInit {
   imagePath = "favicon.ico";
   isDash = true;
   isSignedIn!: boolean;
+  admin_name;
+  errorMessage;
 
   constructor(
     private auth: AuthStateService,
     public router: Router,
     public token: TokenService,
-    public authService :AuthService
+    public authService :AuthService,
+    private http: HttpClient
+
 
   ) { }
 
@@ -36,9 +41,22 @@ export class AdminDashboardComponent implements OnInit {
       localStorage.removeItem('user_type');
       this.signOut()
     }
+    this.getName()
   }
   signOut() {
     this.authService.signOut();
+  }
+  getName(){
+    const body = {id: localStorage.getItem('user_id')}
+    this.http.post<any>(API_URL+'user/name',body).subscribe({
+      next: data => {
+        this.admin_name = data.Name;
+      },
+      error: error => {
+        this.errorMessage = error.message;
+        console.error('There was an error!', this.errorMessage);
+      }
+    })
   }
 
 }
