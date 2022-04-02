4dcac1c62511e03c71b0c4c3a64d700544490dc9
aminabdulkhalek <abdulkhalek632@gmail.com>
2022-04-02 20:28:49 +0300

    admin commissions: added main Auth

--- MultiVendor-FrontEnd/src/app/admin/admin-comissions/admin-comissions.component.ts
+++ MultiVendor-FrontEnd/src/app/admin/admin-comissions/admin-comissions.component.ts
@@ -1,5 +1,10 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { faSignOut, faBell } from '@fortawesome/free-solid-svg-icons';
+import { AuthStateService } from 'src/app/shared/auth-state.service';
+import { AuthService, API_URL } from 'src/app/shared/auth.service';
+import { TokenService } from 'src/app/shared/token.service';
 
 @Component({
   selector: 'app-admin-comissions',
@@ -9,9 +14,45 @@ import { faSignOut, faBell } from '@fortawesome/free-solid-svg-icons';
 export class AdminComissionsComponent implements OnInit {
   logout = faSignOut;
   notification = faBell;
-  constructor() { }
+  isSignedIn!: boolean;
+  admin_name;
+  errorMessage;
+  constructor(
+    private auth: AuthStateService,
+    public router: Router,
+    public token: TokenService,
+    public authService :AuthService,
+    private http: HttpClient
+
+  ) { }
 
   ngOnInit(): void {
+    this.auth.userAuthState.subscribe((val) => {
+      this.isSignedIn = val;
+      if (!val) {
+        this.router.navigate(['login']);
+      }
+    });
+    if (localStorage.getItem('user_type') != '0'){
+      localStorage.removeItem('user_type');
+      this.signOut()
+    }
+    this.getName()
+  }
+  signOut() {
+    this.authService.signOut();
+  }
+  getName(){
+    const body = {id: localStorage.getItem('user_id')}
+    this.http.post<any>(API_URL+'user/name',body).subscribe({
+      next: data => {
+        this.admin_name = data.Name;
+      },
+      error: error => {
+        this.errorMessage = error.message;
+        console.error('There was an error!', this.errorMessage);
+      }
+    })
   }
 
 }
