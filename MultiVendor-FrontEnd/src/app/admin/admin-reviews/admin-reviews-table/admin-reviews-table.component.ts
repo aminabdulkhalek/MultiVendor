93e3372a86ca5f43192bd906233ebd20f234d017
aminabdulkhalek <abdulkhalek632@gmail.com>
2022-04-02 23:32:13 +0300

    admin reviews: get all reviews api call

--- MultiVendor-FrontEnd/src/app/admin/admin-reviews/admin-reviews-table/admin-reviews-table.component.ts
+++ MultiVendor-FrontEnd/src/app/admin/admin-reviews/admin-reviews-table/admin-reviews-table.component.ts
@@ -1,33 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { faStar, faStarHalfAlt, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { API_URL } from 'src/app/shared/auth.service';
 
-export interface Reviews {
-  product_name: string;
-  seller_name: string;
-  customer_name: string;
-  stars: number;
-  flags: number;
-  comment: string;
-  review_status: number
-}
 
-const Product_DATA: Reviews[] = [
-  { product_name: "iPhone 13 Pro", seller_name: "John Doe", customer_name: "John Doe", stars: 4, flags: 0, comment: "Amazing product, great features, the color is very vibrent", review_status: 12 },
-  { product_name: "iPhone 13 Pro", seller_name: "John Doe", customer_name: "John Doe", stars: 4, flags: 0, comment: "Amazing product, great features, the color is very vibrent", review_status: 12 },
-  { product_name: "iPhone 13 Pro", seller_name: "John Doe", customer_name: "John Doe", stars: 4, flags: 0, comment: "Amazing product, great features, the color is very vibrent", review_status: 12 },
-  { product_name: "iPhone 13 Pro", seller_name: "John Doe", customer_name: "John Doe", stars: 4, flags: 0, comment: "Amazing product, great features, the color is very vibrent", review_status: 12 },
-  { product_name: "iPhone 13 Pro", seller_name: "John Doe", customer_name: "John Doe", stars: 4, flags: 0, comment: "Amazing product, great features, the color is very vibrent", review_status: 12 },
-  { product_name: "iPhone 13 Pro", seller_name: "John Doe", customer_name: "John Doe", stars: 4, flags: 0, comment: "Amazing product, great features, the color is very vibrent", review_status: 12 },
-  { product_name: "iPhone 13 Pro", seller_name: "John Doe", customer_name: "John Doe", stars: 4, flags: 0, comment: "Amazing product, great features, the color is very vibrent", review_status: 12 },
-  { product_name: "iPhone 13 Pro", seller_name: "John Doe", customer_name: "John Doe", stars: 4, flags: 0, comment: "Amazing product, great features, the color is very vibrent", review_status: 12 },
-  { product_name: "iPhone 13 Pro", seller_name: "John Doe", customer_name: "John Doe", stars: 4, flags: 0, comment: "Amazing product, great features, the color is very vibrent", review_status: 12 },
-  { product_name: "iPhone 13 Pro", seller_name: "John Doe", customer_name: "John Doe", stars: 4, flags: 0, comment: "Amazing product, great features, the color is very vibrent", review_status: 12 },
-  { product_name: "iPhone 13 Pro", seller_name: "John Doe", customer_name: "John Doe", stars: 4, flags: 0, comment: "Amazing product, great features, the color is very vibrent", review_status: 12 },
-
-];
 
 @Component({
   selector: 'app-admin-reviews-table',
@@ -39,21 +18,69 @@ export class AdminReviewsTableComponent implements OnInit {
   half_star = faStarHalfAlt;
   delete = faTrashAlt
 
-  displayedColumns: string[] = ["product_name", "seller_name", "customer_name", "stars", "flags", "comment", "review_status"];
-  dataSource = new MatTableDataSource<Reviews>(Product_DATA);
-
+  displayedColumns: string[] = ["product_name", "seller_name", "customer_name", "stars", "comment", "review_status"];
+  dataSource = new MatTableDataSource<any>(null);
+  reviews = [];
+  errorMessage;
   @ViewChild(MatPaginator)
   paginator!: MatPaginator;
   @ViewChild(MatSort)
   sort!: MatSort;
 
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
-  }
-  constructor() { }
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getAllReviews();
+  }
+  getAllReviews() {
+    this.http.get<any>(API_URL + 'admin/reviews').subscribe({
+      next: data => {
+        console.log(data)
+        this.dataSource = new MatTableDataSource<any>(data.Reviews);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+        this.reviews = data.Reviews
+        return data
+      },
+      error: error => {
+        this.errorMessage = error.message;
+        console.error('There was an error!', this.errorMessage);
+      }
+    })
+
+  }
+  isEmptyObject() {
+    return (this.reviews.length === 0);
   }
 
+  showReview(product_id) {
+    const body ={
+      product_id :product_id
+    }
+    this.http.post<any>(API_URL + 'admin/show-review', body).subscribe({
+      next: data => {
+        console.log(data)
+      },
+      error: error => {
+        this.errorMessage = error.message;
+        console.error('There was an error!', this.errorMessage);
+      }
+    })
+  }
+  HideReview(product_id) {
+    const body ={
+      product_id :product_id
+    }
+    this.http.post<any>(API_URL + 'admin/hide-review', body).subscribe({
+      next: data => {
+        console.log(data)
+      },
+      error: error => {
+        this.errorMessage = error.message;
+        console.error('There was an error!', this.errorMessage);
+      }
+    })
+  }
 }
+
