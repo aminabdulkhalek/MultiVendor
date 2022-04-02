f3dca312bfa0f4762f2065580adcd08846273505
aminabdulkhalek <abdulkhalek632@gmail.com>
2022-04-02 23:59:27 +0300

    admin transactions: get balances request

--- MultiVendor-FrontEnd/src/app/admin/admin-transactions/admin-transactions-table/admin-transactions-table.component.ts
+++ MultiVendor-FrontEnd/src/app/admin/admin-transactions/admin-transactions-table/admin-transactions-table.component.ts
@@ -1,32 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { faStar, faStarHalfAlt, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { API_URL } from 'src/app/shared/auth.service';
 
 
-export interface Transactions {
-  logo: string;
-  seller_name: string;
-  commission_rate: number;
-  total_sales: number;
-  recevied_amount: number;
-  commission_amount: number;
-  remaining_amount: number;
-  last_paid_amount: number;
-}
 
-const Product_DATA: Transactions[] = [
-  { logo: "/assets/logo.png", seller_name: "John Doe", commission_rate: 20.0, total_sales: 1050.00, recevied_amount: 0.00, commission_amount: 0.00, remaining_amount: 1050.00, last_paid_amount: 0.00 },
-  { logo: "/assets/logo.png", seller_name: "John Doe", commission_rate: 20.0, total_sales: 1050.00, recevied_amount: 0.00, commission_amount: 0.00, remaining_amount: 1050.00, last_paid_amount: 0.00 },
-  { logo: "/assets/logo.png", seller_name: "John Doe", commission_rate: 20.0, total_sales: 1050.00, recevied_amount: 0.00, commission_amount: 0.00, remaining_amount: 1050.00, last_paid_amount: 0.00 },
-  { logo: "/assets/logo.png", seller_name: "John Doe", commission_rate: 20.0, total_sales: 1050.00, recevied_amount: 0.00, commission_amount: 0.00, remaining_amount: 1050.00, last_paid_amount: 0.00 },
-  { logo: "/assets/logo.png", seller_name: "John Doe", commission_rate: 20.0, total_sales: 1050.00, recevied_amount: 0.00, commission_amount: 0.00, remaining_amount: 1050.00, last_paid_amount: 0.00 },
-  { logo: "/assets/logo.png", seller_name: "John Doe", commission_rate: 20.0, total_sales: 1050.00, recevied_amount: 0.00, commission_amount: 0.00, remaining_amount: 1050.00, last_paid_amount: 0.00 },
-  { logo: "/assets/logo.png", seller_name: "John Doe", commission_rate: 20.0, total_sales: 1050.00, recevied_amount: 0.00, commission_amount: 0.00, remaining_amount: 1050.00, last_paid_amount: 0.00 },
-  { logo: "/assets/logo.png", seller_name: "John Doe", commission_rate: 20.0, total_sales: 1050.00, recevied_amount: 0.00, commission_amount: 0.00, remaining_amount: 1050.00, last_paid_amount: 0.00 },
-
-];
 @Component({
   selector: 'app-admin-transactions-table',
   templateUrl: './admin-transactions-table.component.html',
@@ -37,21 +18,40 @@ export class AdminTransactionsTableComponent implements OnInit {
   half_star = faStarHalfAlt;
   delete = faTrashAlt
 
-  displayedColumns: string[] = ["logo", "seller_name","commission_rate","total_sales","recevied_amount","commission_amount","remaining_amount","last_paid_amount"];
-  dataSource = new MatTableDataSource<Transactions>(Product_DATA);
+  displayedColumns: string[] = ["logo", "seller_name","commission_rate","total_sales","recevied_amount","commission_amount","remaining_amount"];
 
+  dataSource = new MatTableDataSource<any>(null);
+  vendors = [];
+  errorMessage;
   @ViewChild(MatPaginator)
   paginator!: MatPaginator;
   @ViewChild(MatSort)
   sort!: MatSort;
 
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
-  }
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getBalances();
+  }
+  getBalances() {
+    this.http.get<any>(API_URL + 'admin/balances').subscribe({
+      next: data => {
+        // console.log(data)
+        this.dataSource = new MatTableDataSource<any>(data.Balances);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+        this.vendors = data.Balances;
+        return data
+      },
+      error: error => {
+        this.errorMessage = error.message;
+        console.error('There was an error!', this.errorMessage);
+      }
+    })
+
+  }
+  isEmptyObject() {
+    return (this.vendors.length === 0);
   }
 
 }
