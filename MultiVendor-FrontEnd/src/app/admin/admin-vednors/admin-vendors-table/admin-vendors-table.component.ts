b67cca96f3833c057935a08e9076db28d177d434
aminabdulkhalek <abdulkhalek632@gmail.com>
2022-04-02 15:40:13 +0300

    admin vendors: get data - approve/disapprove - changed status section - styling

--- MultiVendor-FrontEnd/src/app/admin/admin-vednors/admin-vendors-table/admin-vendors-table.component.ts
+++ MultiVendor-FrontEnd/src/app/admin/admin-vednors/admin-vendors-table/admin-vendors-table.component.ts
@@ -1,55 +1,85 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { faStar, faStarHalfAlt, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { API_URL } from 'src/app/shared/auth.service';
 
 
-export interface Vendors {
-  logo: string;
-  seller_name: string;
-  seller_email: string;
-  flags: number;
-  phone: string;
-  seller_status: number;
-}
-
-const Product_DATA: Vendors[] = [
-  { logo: "/assets/logo.png", seller_name: "John Doe", seller_email: "example@gmail.com", flags: 5, phone: "01789325", seller_status: 0 },
-  { logo: "/assets/logo.png", seller_name: "John Doe", seller_email: "example@gmail.com", flags: 5, phone: "01789325", seller_status: 0 },
-  { logo: "/assets/logo.png", seller_name: "John Doe", seller_email: "example@gmail.com", flags: 5, phone: "01789325", seller_status: 0 },
-  { logo: "/assets/logo.png", seller_name: "John Doe", seller_email: "example@gmail.com", flags: 5, phone: "01789325", seller_status: 0 },
-  { logo: "/assets/logo.png", seller_name: "John Doe", seller_email: "example@gmail.com", flags: 5, phone: "01789325", seller_status: 0 },
-  { logo: "/assets/logo.png", seller_name: "John Doe", seller_email: "example@gmail.com", flags: 5, phone: "01789325", seller_status: 0 },
-  { logo: "/assets/logo.png", seller_name: "John Doe", seller_email: "example@gmail.com", flags: 5, phone: "01789325", seller_status: 0 },
-  { logo: "/assets/logo.png", seller_name: "John Doe", seller_email: "example@gmail.com", flags: 5, phone: "01789325", seller_status: 0 },
-
-];
 @Component({
   selector: 'app-admin-vendors-table',
   templateUrl: './admin-vendors-table.component.html',
   styleUrls: ['./admin-vendors-table.component.scss']
 })
 export class AdminVendorsTableComponent implements OnInit {
+  dataSource = new MatTableDataSource<any>(null);
   star = faStar;
   half_star = faStarHalfAlt;
   delete = faTrashAlt
-
+  errorMessage;
   displayedColumns: string[] = ["logo", "seller_name", "seller_email", "flags", "phone", "seller_status"];
-  dataSource = new MatTableDataSource<Vendors>(Product_DATA);
-
+  vendors =[];
   @ViewChild(MatPaginator)
   paginator!: MatPaginator;
   @ViewChild(MatSort)
   sort!: MatSort;
 
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
-  }
-  constructor() { }
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getAllVendors();
+  }
+  getAllVendors() {
+    this.http.get<any>(API_URL + 'admin/vendors').subscribe({
+      next: data => {
+        console.log(data)
+        this.dataSource = new MatTableDataSource<any>(data.Vendors);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+        this.vendors = data.Vendors
+        return data
+      },
+      error: error => {
+        this.errorMessage = error.message;
+        console.error('There was an error!', this.errorMessage);
+      }
+    })
+
+  }
+  isEmptyObject() {
+    return (this.vendors.length === 0);
   }
 
+  approveProduct(product_id) {
+    const body ={
+      product_id :product_id
+    }
+    this.http.post<any>(API_URL + 'admin/approve-vendor', body).subscribe({
+      next: data => {
+        this.getAllVendors()
+      },
+      error: error => {
+        this.errorMessage = error.message;
+        console.error('There was an error!', this.errorMessage);
+      }
+    })
+  }
+  disapproveProduct(product_id) {
+    const body ={
+      product_id :product_id
+    }
+    this.http.post<any>(API_URL + 'admin/disapprove-vendor', body).subscribe({
+      next: data => {
+        this.getAllVendors()
+      },
+      error: error => {
+        this.errorMessage = error.message;
+        console.error('There was an error!', this.errorMessage);
+      }
+    })
+  }
 }
+
+
