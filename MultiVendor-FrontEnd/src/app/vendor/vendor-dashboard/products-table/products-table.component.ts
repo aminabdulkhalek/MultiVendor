e53c965364ac3df638b562c414b37d4b753768ec
aminabdulkhalek <abdulkhalek632@gmail.com>
2022-04-03 13:41:26 +0300

    vendor dashbaord products box: linked to backend

--- MultiVendor-FrontEnd/src/app/vendor/vendor-dashboard/products-table/products-table.component.ts
+++ MultiVendor-FrontEnd/src/app/vendor/vendor-dashboard/products-table/products-table.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { API_URL } from 'src/app/shared/auth.service';
 
 @Component({
   selector: 'app-products-table',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products-table.component.scss']
 })
 export class ProductsTableComponent implements OnInit {
-
-  constructor() { }
+  products;
+  nb_products;
+  errorMessage;
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.initBox()
   }
-
+  initBox(){
+    this.http.get<any>(API_URL+'vendor/top-selling').subscribe({
+      next: data => {
+        this.products = data.top_selling;
+      },
+      error: error => {
+        this.errorMessage = error.message;
+        console.error('There was an error!', this.errorMessage);
+      }
+    })
+    this.http.get<any>(API_URL+'vendor/nb-products').subscribe({
+      next: data => {
+        this.nb_products = data.products;
+      },
+      error: error => {
+        this.errorMessage = error.message;
+        console.error('There was an error!', this.errorMessage);
+      }
+    })
+  }
 }
