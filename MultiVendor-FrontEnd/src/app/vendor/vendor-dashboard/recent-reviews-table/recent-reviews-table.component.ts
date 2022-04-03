3dc1eabf4dbdeb5a7bd1da599a8c6f0a8ce3a223
aminabdulkhalek <abdulkhalek632@gmail.com>
2022-04-03 14:42:28 +0300

    vendor recent reviews: linked to backend + updated the component

--- MultiVendor-FrontEnd/src/app/vendor/vendor-dashboard/recent-reviews-table/recent-reviews-table.component.ts
+++ MultiVendor-FrontEnd/src/app/vendor/vendor-dashboard/recent-reviews-table/recent-reviews-table.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { API_URL } from 'src/app/shared/auth.service';
 
 @Component({
   selector: 'app-recent-reviews-table',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recent-reviews-table.component.scss']
 })
 export class RecentReviewsTableComponent implements OnInit {
-
-  constructor() { }
+  reviews;
+  errorMessage
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get<any>(API_URL+'vendor/recent-reviews').subscribe({
+      next: data => {
+        console.log(data)
+        this.reviews = data.Reviews;
+      },
+      error: error => {
+        this.errorMessage = error.message;
+        console.error('There was an error!', this.errorMessage);
+      }
+    })
   }
 
 }
