20cbabb14e5b9bf0b275debe24004d36c52f014c
aminabdulkhalek <abdulkhalek632@gmail.com>
2022-04-04 01:09:52 +0300

    add and remove product from wishlist in list display

--- MultiVendor-FrontEnd/src/app/Customer/products/product-list-view/product-list-view.component.ts
+++ MultiVendor-FrontEnd/src/app/Customer/products/product-list-view/product-list-view.component.ts
@@ -1,6 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { faStar, faStarHalfAlt } from '@fortawesome/free-solid-svg-icons';
+import { API_URL } from 'src/app/shared/auth.service';
 
 @Component({
   selector: 'app-product-list-view',
@@ -10,14 +12,49 @@ import { faStar, faStarHalfAlt } from '@fortawesome/free-solid-svg-icons';
 export class ProductListViewComponent implements OnInit {
   @Input()
   product;
+  @Input()
+  index;
+  
+  errorMessage
   star=faStar;
   half_star=faStarHalfAlt;
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+    private http: HttpClient) { }
 
   ngOnInit(): void {
     
   }
   redirectToProduct(){
     this.router.navigate(['product'])
+  }
+  addToWishlist(){
+    document.getElementById(`love0${this.index}`).classList.add('hide')
+    document.getElementById(`love1${this.index}`).classList.remove('hide')
+    const body = {
+      'product_id':this.product.id
+    }
+    this.http.post<any>(API_URL+'customer/love-product',body).subscribe({
+      next: data => {
+      },
+      error: error => {
+        this.errorMessage = error.message;
+        console.error('There was an error!', this.errorMessage);
+      }
+    })
+  }
+  removeFromWishlist(){
+    document.getElementById(`love1${this.index}`).classList.add('hide')
+    document.getElementById(`love0${this.index}`).classList.remove('hide')
+    const body = {
+      'product_id':this.product.id
+    }
+    this.http.post<any>(API_URL+'customer/remove-product-wishlist',body).subscribe({
+      next: data => {
+      },
+      error: error => {
+        this.errorMessage = error.message;
+        console.error('There was an error!', this.errorMessage);
+      }
+    })
   }
 }
