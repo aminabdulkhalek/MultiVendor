be0fa819cf5d42d1eb2106ff57ef677cc86ace93
aminabdulkhalek <abdulkhalek632@gmail.com>
2022-04-03 23:38:01 +0300

    customer dashbaord: added main auth

--- MultiVendor-FrontEnd/src/app/Customer/customer-dashboard/customer-dashboard.component.ts
+++ MultiVendor-FrontEnd/src/app/Customer/customer-dashboard/customer-dashboard.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { faSignOut, faBell, faHeart, faShoppingCart, faUserCircle, faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
-import {Router} from "@angular/router"
+import { Router } from "@angular/router"
+import { HttpClient } from '@angular/common/http';
+import { AuthStateService } from 'src/app/shared/auth-state.service';
+import { AuthService } from 'src/app/shared/auth.service';
+import { TokenService } from 'src/app/shared/token.service';
 
 @Component({
   selector: 'app-customer-dashboard',
@@ -8,14 +12,34 @@ import {Router} from "@angular/router"
   styleUrls: ['./customer-dashboard.component.scss']
 })
 export class CustomerDashboardComponent implements OnInit {
-  logout=faSignOut;
+  logout = faSignOut;
   account = faUserCircle;
   wishlist = faHeart;
-  cart= faShoppingCart;
-  shopping_cart= faShoppingBasket;
-  constructor(private router: Router) { }
+  cart = faShoppingCart;
+  shopping_cart = faShoppingBasket;
+  isSignedIn!: boolean;
+
+  constructor(
+    private auth: AuthStateService,
+    public router: Router,
+    public token: TokenService,
+    public authService: AuthService,
+    private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.auth.userAuthState.subscribe((val) => {
+      this.isSignedIn = val;
+      if (!val) {
+        this.router.navigate(['login']);
+      }
+    });
+    if (localStorage.getItem('user_type') != '2') {
+      localStorage.removeItem('user_type');
+      this.signOut()
+    }
+  }
+  signOut() {
+    this.authService.signOut();
   }
   productsPage() {
     this.router.navigate(['products'])
