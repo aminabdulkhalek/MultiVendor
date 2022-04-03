d3fb90c3beb2843d434e8f84419e2e0f77de91f9
aminabdulkhalek <abdulkhalek632@gmail.com>
2022-04-03 13:31:52 +0300

    vendor dashbaord: added main auth and get info for navbar

--- MultiVendor-FrontEnd/src/app/vendor/vendor-dashboard/vendor-dashboard.component.ts
+++ MultiVendor-FrontEnd/src/app/vendor/vendor-dashboard/vendor-dashboard.component.ts
@@ -1,5 +1,10 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { faBell, faSignOut } from '@fortawesome/free-solid-svg-icons';
+import { AuthStateService } from 'src/app/shared/auth-state.service';
+import { AuthService, API_URL } from 'src/app/shared/auth.service';
+import { TokenService } from 'src/app/shared/token.service';
 
 
 @Component({
@@ -10,13 +15,61 @@ import { faBell, faSignOut } from '@fortawesome/free-solid-svg-icons';
 export class VendorDashboardComponent implements OnInit {
   logout=faSignOut;
   notification = faBell;
-  imagePath = "favicon.ico";
+  imagePath = "";
   isDash = true;
-  constructor() { }
+  isSignedIn!: boolean;
+  vendor_name;
+  errorMessage;
+
+  constructor(
+    private auth: AuthStateService,
+    public router: Router,
+    public token: TokenService,
+    public authService :AuthService,
+    private http: HttpClient
+
+
+  ) { }
 
   ngOnInit(): void {
+    this.auth.userAuthState.subscribe((val) => {
+      this.isSignedIn = val;
+      if (!val) {
+        this.router.navigate(['login']);
+      }
+    });
+    if (localStorage.getItem('user_type') != '1'){
+      localStorage.removeItem('user_type');
+      this.signOut()
+    }
+    this.getInfo()
   }
-   
+  signOut() {
+    this.authService.signOut();
+  }
+  getInfo(){
+    const body = {id: localStorage.getItem('user_id')}
+    this.http.post<any>(API_URL+'user/name',body).subscribe({
+      next: data => {
+        this.vendor_name = data.Name;
+      },
+      error: error => {
+        this.errorMessage = error.message;
+        console.error('There was an error!', this.errorMessage);
+      }
+    })
+    this.http.get<any>(API_URL+'vendor/get-profile').subscribe({
+      next: data => {
+        this.imagePath = data.vendor.logo;
+      },
+      error: error => {
+        this.errorMessage = error.message;
+        console.error('There was an error!', this.errorMessage);
+      }
+    })
+  }
+
 }
+   
 
 
