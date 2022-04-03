208363a28365249156fd09d2cd67b1bca29b5af7
aminabdulkhalek <abdulkhalek632@gmail.com>
2022-04-03 16:55:12 +0300

    vendor reviews: linked to backend & updated component

--- MultiVendor-FrontEnd/src/app/vendor/vendor-reviews/vendor-reviews-table/vendor-reviews-table.component.ts
+++ MultiVendor-FrontEnd/src/app/vendor/vendor-reviews/vendor-reviews-table/vendor-reviews-table.component.ts
@@ -1,31 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { faFile, faImage, faStar, faStarHalfAlt, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { API_URL } from 'src/app/shared/auth.service';
 
-export interface Reviews {
-  id: number;
-  product_name: string;
-  customer_name: string;
-  stars: number;
-  reviews_comment: string;
-  review_date: Date;
-  
-}
-const Product_DATA: Reviews[] = [
-  { id: 1, customer_name: "Petter Yamout", product_name: "Blue Jeans", stars: 3.5, reviews_comment: "Amazing product, great features, the color is very cool ", review_date: new Date('12-3-2020') },
-  { id: 2, customer_name: 'red Jeans', product_name: "Blue Jeans", stars: 4, reviews_comment: "Amazing product, great features, the color is very cool ", review_date: new Date('2-3-2020') },
-  { id: 3, customer_name: 'green Jeans', product_name: "Blue Jeans", stars: 3.5, reviews_comment: "Amazing product, great features, the color is very cool ", review_date: new Date('12-4-2020') },
-  { id: 4, customer_name: 'John Doe', product_name: "Blue Jeans", stars: 3.5, reviews_comment: "Amazing product, great features, the color is very cool ", review_date: new Date('1-3-2020') },
-  { id: 5, customer_name: "Mike Itani", product_name: "Blue Jeans", stars: 3.5, reviews_comment: "Amazing product, great features, the color is very cool ", review_date: new Date('3-4-2021') },
-  { id: 6, customer_name: "Sarah Johnson", product_name: "Blue Jeans", stars: 3.5, reviews_comment: "Amazing product, great features, the color is very cool ", review_date: new Date('1-10-2020') },
-  { id: 7, customer_name: "Sarah Johnson", product_name: "Blue Jeans", stars: 3.5, reviews_comment: "Amazing product, great features, the color is very cool ", review_date: new Date('4-3-2020') },
-  { id: 8, customer_name: "Sarah Johnson", product_name: "Blue Jeans", stars: 4, reviews_comment: "Amazing product, great features, the color is very cool ", review_date: new Date('12-3-2019') },
-  { id: 9, customer_name: "Sarah Johnson", product_name: "Blue Jeans", stars: 3.5, reviews_comment: "Amazing product, great features, the color is very cool ", review_date: new Date('12-3-2018') },
-  { id: 10, customer_name: "Sarah Johnson", product_name: "Blue Jeans", stars: 3.5, reviews_comment: "Amazing product, great features, the color is very cool ", review_date: new Date('12-3-2017') },
-  { id: 11, customer_name: "Sarah Johnson", product_name: "Blue Jeans", stars: 3.5, reviews_comment: "Amazing product, great features, the color is very cool ", review_date: new Date('12-3-2016') },
-];
+
 @Component({
   selector: 'app-vendor-reviews-table',
   templateUrl: './vendor-reviews-table.component.html',
@@ -36,19 +17,37 @@ export class VendorReviewsTableComponent implements OnInit {
   half_star = faStarHalfAlt;
   delete = faTrashAlt
   displayedColumns: string[] = ['id', 'product_name', 'customer_name', 'stars', 'reviews_comment', 'review_date'];
-  dataSource = new MatTableDataSource<Reviews>(Product_DATA);
-
+  dataSource = new MatTableDataSource<any>(null);
+  reviews = [];
+  errorMessage;
   @ViewChild(MatPaginator)
   paginator!: MatPaginator;
   @ViewChild(MatSort)
   sort!: MatSort;
 
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
-  }
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getAllReviews();
   }
+  getAllReviews() {
+    this.http.get<any>(API_URL + 'vendor/reviews').subscribe({
+      next: data => {
+        this.dataSource = new MatTableDataSource<any>(data.Reviews);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+        this.reviews = data.Reviews
+        return data
+      },
+      error: error => {
+        this.errorMessage = error.message;
+        console.error('There was an error!', this.errorMessage);
+      }
+    })
 
+  }
+  isEmptyObject() {
+    return (this.reviews.length === 0);
+  }
 }
