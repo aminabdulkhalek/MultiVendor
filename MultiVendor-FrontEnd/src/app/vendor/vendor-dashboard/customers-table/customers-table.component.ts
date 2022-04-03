48715f7fc21a1f42bebfb9b6e096d9f6f539bae5
aminabdulkhalek <abdulkhalek632@gmail.com>
2022-04-03 13:45:44 +0300

    vendor dashbaord customers box: linked to backend

--- MultiVendor-FrontEnd/src/app/vendor/vendor-dashboard/customers-table/customers-table.component.ts
+++ MultiVendor-FrontEnd/src/app/vendor/vendor-dashboard/customers-table/customers-table.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { API_URL } from 'src/app/shared/auth.service';
 
 @Component({
   selector: 'app-customers-table',
@@ -7,9 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomersTableComponent implements OnInit {
 
-  constructor() { }
+  total_customers;
+  this_customers;
+  last_customers;
+  errorMessage;
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.initBox()
+
+  }
+
+  initBox(){
+    this.http.get<any>(API_URL+'vendor/nb-customers').subscribe({
+      next: data => {
+        this.total_customers = data.nbCustomers;
+      },
+      error: error => {
+        this.errorMessage = error.message;
+        console.error('There was an error!', this.errorMessage);
+      }
+    })
+    this.http.get<any>(API_URL+'vendor/this-month-customers').subscribe({
+      next: data => {
+        this.this_customers = data.this_month_customers;
+      },
+      error: error => {
+        this.errorMessage = error.message;
+        console.error('There was an error!', this.errorMessage);
+      }
+    })
+    this.http.get<any>(API_URL+'vendor/last-month-customers').subscribe({
+      next: data => {
+        this.last_customers = data.last_month_customers;
+      },
+      error: error => {
+        this.errorMessage = error.message;
+        console.error('There was an error!', this.errorMessage);
+      }
+    })
   }
 
 }
