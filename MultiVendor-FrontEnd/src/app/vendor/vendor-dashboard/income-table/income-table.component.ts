b505f775429454c234750dd26e53152a4f241261
aminabdulkhalek <abdulkhalek632@gmail.com>
2022-04-03 14:48:38 +0300

    vendor income box: linked to backend + updated the component

--- MultiVendor-FrontEnd/src/app/vendor/vendor-dashboard/income-table/income-table.component.ts
+++ MultiVendor-FrontEnd/src/app/vendor/vendor-dashboard/income-table/income-table.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { API_URL } from 'src/app/shared/auth.service';
 
 @Component({
   selector: 'app-income-table',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./income-table.component.scss']
 })
 export class IncomeTableComponent implements OnInit {
+ total_sales;
+ total_payout;
+ errorMessage;
 
-  constructor() { }
+ constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get<any>(API_URL+'vendor/total-payout').subscribe({
+      next: data => {
+        this.total_payout = data.total_payout;
+      },
+      error: error => {
+        this.errorMessage = error.message;
+        console.error('There was an error!', this.errorMessage);
+      }
+    })
+    this.http.get<any>(API_URL+'vendor/total-sales').subscribe({
+      next: data => {
+        this.total_sales = data.total_Sales;
+      },
+      error: error => {
+        this.errorMessage = error.message;
+        console.error('There was an error!', this.errorMessage);
+      }
+    })
   }
 
 }
