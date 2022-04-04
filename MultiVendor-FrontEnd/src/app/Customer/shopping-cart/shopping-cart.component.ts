f27a59b75ac4686b0fbc9207274add52eabfe347
aminabdulkhalek <abdulkhalek632@gmail.com>
2022-04-04 05:22:59 +0300

    added main auth to shopping cart

--- MultiVendor-FrontEnd/src/app/Customer/shopping-cart/shopping-cart.component.ts
+++ MultiVendor-FrontEnd/src/app/Customer/shopping-cart/shopping-cart.component.ts
@@ -1,7 +1,11 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
 import { faSignOut, faUserCircle, faHeart, faShoppingCart, faShoppingBasket, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { AuthStateService } from 'src/app/shared/auth-state.service';
+import { AuthService } from 'src/app/shared/auth.service';
+import { TokenService } from 'src/app/shared/token.service';
 
 
 @Component({
@@ -18,17 +22,36 @@ export class ShoppingCartComponent implements OnInit {
   phone = faPhone;
   value=0;
   items=[1,2,3]
-  constructor(private router: Router) { }
+  isSignedIn: boolean;
+  constructor(
+    private auth: AuthStateService,
+    public router: Router,
+    public token: TokenService,
+    public authService: AuthService,
+    private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.auth.userAuthState.subscribe((val) => {
+      this.isSignedIn = val;
+      if (!val) {
+        this.router.navigate(['login']);
+      }
+    });
+    if (localStorage.getItem('user_type') != '2') {
+      localStorage.removeItem('user_type');
+      this.signOut()
+    }
+  }
+  signOut() {
+    this.authService.signOut();
   }
   handleMinus() {
     if (this.value>0) {
-      this.value--;  
+      this.value--;
     }
   }
   handlePlus() {
-    this.value++;    
+    this.value++;
   }
   redirectToCheckout(){
     this.router.navigate(['checkout'])
