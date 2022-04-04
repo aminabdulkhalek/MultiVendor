18bf0f9ddf056e0d381da1955453b606029b73e7
aminabdulkhalek <abdulkhalek632@gmail.com>
2022-04-04 04:17:04 +0300

    added main auth to shops component

--- MultiVendor-FrontEnd/src/app/Customer/shops/shops.component.ts
+++ MultiVendor-FrontEnd/src/app/Customer/shops/shops.component.ts
@@ -1,5 +1,10 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { faSignOut, faUserCircle, faHeart, faShoppingCart, faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
+import { AuthStateService } from 'src/app/shared/auth-state.service';
+import { AuthService, API_URL } from 'src/app/shared/auth.service';
+import { TokenService } from 'src/app/shared/token.service';
 
 @Component({
   selector: 'app-shops',
@@ -13,9 +18,32 @@ export class ShopsComponent implements OnInit {
   cart= faShoppingCart;
   shopping_cart= faShoppingBasket;
   shops=[1,2,3,4,5,6,7,8,9]
-  constructor() { }
+  isSignedIn: boolean;
+  constructor(
+    private auth: AuthStateService,
+    public router: Router,
+    public token: TokenService,
+    public authService: AuthService,
+    private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.auth.userAuthState.subscribe((val) => {
+      this.isSignedIn = val;
+      if (!val) {
+        this.router.navigate(['login']);
+      }
+    });
+    if (localStorage.getItem('user_type') != '2') {
+      localStorage.removeItem('user_type');
+      this.signOut()
+    }
   }
+  signOut() {
+    this.authService.signOut();
+  }
+  productsPage() {
+    this.router.navigate(['products'])
+  }
+
 
 }
