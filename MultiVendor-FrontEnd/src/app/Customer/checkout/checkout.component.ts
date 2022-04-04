a179b56c5792648c855a4e0a8e229cb8c836aa3f
aminabdulkhalek <abdulkhalek632@gmail.com>
2022-04-04 06:19:56 +0300

    added main auth to checkout

--- MultiVendor-FrontEnd/src/app/Customer/checkout/checkout.component.ts
+++ MultiVendor-FrontEnd/src/app/Customer/checkout/checkout.component.ts
@@ -1,6 +1,11 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 import { faSignOut, faUserCircle, faHeart, faShoppingCart, faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
+import { AuthStateService } from 'src/app/shared/auth-state.service';
+import { AuthService } from 'src/app/shared/auth.service';
+import { TokenService } from 'src/app/shared/token.service';
 import { OrderPlacedModalComponent } from './order-placed-modal/order-placed-modal.component';
 
 @Component({
@@ -14,12 +19,32 @@ export class CheckoutComponent implements OnInit {
   wishlist = faHeart;
   cart = faShoppingCart;
   shopping_cart = faShoppingBasket;
-  constructor(public dialog: MatDialog) { }
+  isSignedIn: boolean;
+  constructor(public dialog: MatDialog,private auth: AuthStateService,
+    public router: Router,
+    public token: TokenService,
+    public authService: AuthService,
+    private http: HttpClient) { }
   openDialog() {
     this.dialog.open(OrderPlacedModalComponent);
   }
 
   ngOnInit(): void {
+    this.auth.userAuthState.subscribe((val) => {
+      this.isSignedIn = val;
+      if (!val) {
+        this.router.navigate(['login']);
+      }
+    });
+    if (localStorage.getItem('user_type') != '2') {
+      localStorage.removeItem('user_type');
+      this.signOut()
+    }
+
   }
+  signOut() {
+    this.authService.signOut();
+  }
+
 
 }
