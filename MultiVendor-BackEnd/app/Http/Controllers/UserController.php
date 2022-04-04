5979c3832715ba514bffec381e5f8ff01c69d175
aminabdulkhalek <abdulkhalek632@gmail.com>
2022-04-04 03:44:40 +0300

    updated get product info in userController

--- MultiVendor-BackEnd/app/Http/Controllers/UserController.php
+++ MultiVendor-BackEnd/app/Http/Controllers/UserController.php
@@ -7,6 +7,10 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use App\Models\Product;
 use App\Models\Category;
+use App\Models\ProductFlag;
+use App\Models\Review;
+use App\Models\Vendor;
+use App\Models\Customer;
 
 class UserController extends Controller
 {
@@ -41,7 +45,29 @@ class UserController extends Controller
 
     public function getProduct(Request $request){
         $product = Product::where('id','=',$request->product_id)->get()->first();
+        $vendor = Vendor::where('id','=',$product->vendor_id)->get()->first();
+        $vendor_name = User::Where('id','=',$vendor->user_id)->get('name')->first()->name;
+        $flags = ProductFlag::where('product_id','=',$product->id)->get()->count();
+            array_add($product, 'flag', $flags);
+            array_add($product, 'product_owner', $vendor_name);
+            $reviews = Review::where('product_id','=',$product->id)->get();
+            $average_stars = 0;
+            $updated_reviews=[];
+            if (count($reviews)>0) {
+                foreach ($reviews as $review) {
+                $average_stars = $average_stars+ $review->stars;
+                $customer = Customer::where('id','=',$review->customer_id)->get()->first();
+                $customer_name = User::where('id','=',$customer->user_id)->get('name')->first()->name;
 
+                array_add($review,'customer_name',$customer_name);
+                array_push($updated_reviews,$review);
+                }
+                $average_stars = $average_stars/count($reviews);
+                array_add($product, 'average_reviews', $average_stars);
+                array_add($product, 'reviewz', $updated_reviews);
+                array_add($product, 'total_reviews', count($reviews));
+            }
+            array_add($product, 'average_reviews', 0);
         return response()->json([
             'product' =>$product
         ], 200);
