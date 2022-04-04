ff57f53e78b3ba8d0fa64eb1ba2d951be54f598f
aminabdulkhalek <abdulkhalek632@gmail.com>
2022-04-04 06:41:45 +0300

    checkout: placeorder function + add billing info

--- MultiVendor-FrontEnd/src/app/Customer/checkout/checkout.component.ts
+++ MultiVendor-FrontEnd/src/app/Customer/checkout/checkout.component.ts
@@ -4,7 +4,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { faSignOut, faUserCircle, faHeart, faShoppingCart, faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
 import { AuthStateService } from 'src/app/shared/auth-state.service';
-import { AuthService } from 'src/app/shared/auth.service';
+import { API_URL, AuthService } from 'src/app/shared/auth.service';
 import { TokenService } from 'src/app/shared/token.service';
 import { OrderPlacedModalComponent } from './order-placed-modal/order-placed-modal.component';
 
@@ -20,7 +20,18 @@ export class CheckoutComponent implements OnInit {
   cart = faShoppingCart;
   shopping_cart = faShoppingBasket;
   isSignedIn: boolean;
-  constructor(public dialog: MatDialog,private auth: AuthStateService,
+
+  name;
+  country;
+  address;
+  city;
+  state;
+  zip_code;
+  phone;
+  email;
+  errorMessage: any;
+
+  constructor(public dialog: MatDialog, private auth: AuthStateService,
     public router: Router,
     public token: TokenService,
     public authService: AuthService,
@@ -46,5 +57,37 @@ export class CheckoutComponent implements OnInit {
     this.authService.signOut();
   }
 
+  placeOrder() {
+
+    this.http.get<any>(API_URL + 'customer/place-order').subscribe({
+      next: data => {
+        console.log(data)
+        const body = {
+          'order_id': data.order.id,
+          'country': this.country,
+          'city': this.city,
+          'state': this.state,
+          'street_address': this.address,
+          'email': this.email,
+          'zip_code': this.zip_code,
+          'phone': this.phone
+        }
+        this.http.post<any>(API_URL + 'customer/add-billing-info',body).subscribe({
+          next: data => {
+            console.log(data)
+          },
+          error: error => {
+            this.errorMessage = error.message;
+            console.error('There was an error!', this.errorMessage);
+          }
+        })
+      },
+      error: error => {
+        this.errorMessage = error.message;
+        console.error('There was an error!', this.errorMessage);
+      }
+    })
+
+  }
 
 }
